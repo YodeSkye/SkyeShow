d2bf7473a68f75dfdb9a2c944675c043befeeddc
YodeSkye <YodeSkye@outlook.com>
2026-07-08 19:20:50 -0400

    Remove command-line argument handling from MyApplication
    Eliminated the _pendingArgs field, ProcessCommandLine method, and OnStartupNextInstance override from MyApplication. Startup logic is simplified by removing command-line argument processing, with no changes to LibVLCSharp initialization or splash screen display.

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -5,12 +5,11 @@ Imports LibVLCSharp.[Shared]
 Namespace My
 	Partial Friend Class MyApplication
 
-		'App Declarations
+		' Declarations
 		Friend CurrentProcess As Diagnostics.Process = Diagnostics.Process.GetCurrentProcess
 		Friend AlternateStart As Boolean = False
-		Private _pendingArgs As ReadOnlyCollection(Of String)
 
-		'App Events
+		' Events
 		Public Sub New()
 			MyBase.New(ApplicationServices.AuthenticationMode.Windows)
 			If My.Computer.Keyboard.ShiftKeyDown Then AlternateStart = True
@@ -29,50 +28,22 @@ Namespace My
 			splash.Refresh()
 
 			LibVLCSharp.Shared.Core.Initialize()
-            Try
-                Using vlcwarmup As New LibVLC()
-                    'Do Nothing - Just Warm Up LibVLC
-                End Using
-            Catch
-            End Try
+			Try
+				Using vlcwarmup As New LibVLC()
+					'Do Nothing - Just Warm Up LibVLC
+				End Using
+			Catch
+			End Try
 
-            splash.Close()
-            splash.Dispose()
+			splash.Close()
+			splash.Dispose()
 
-			_pendingArgs = e.CommandLine
 			Return True
 		End Function
-		Protected Overrides Sub OnStartupNextInstance(e As ApplicationServices.StartupNextInstanceEventArgs)
-			e.BringToForeground = False
-			ProcessCommandLine(e.CommandLine)
-		End Sub
 		Protected Overrides Sub OnCreateMainForm()
 			App.FrmMain = New MainForm
 			Me.MainForm = App.FrmMain
 			My.App.Initialize()
-			If _pendingArgs IsNot Nothing Then ProcessCommandLine(_pendingArgs)
-		End Sub
-
-		'App Procedures
-		Private Sub ProcessCommandLine(ByRef commandline As Collections.ObjectModel.ReadOnlyCollection(Of String))
-			If commandline.Count > 0 Then
-				Try
-					If commandline.Count = 1 Then
-						If String.IsNullOrEmpty(commandline(0)) Then : Throw New Exception
-						Else
-							My.App.CommandLinePath = commandline(0)
-							If Not My.App.IsGeneratingFileList Then My.App.ShowVideoFromCommandLine()
-						End If
-					Else : Throw New Exception
-					End If
-				Catch
-					Dim commandlineparameters As String = String.Empty
-					For Each s As String In commandline : commandlineparameters += IIf(String.IsNullOrEmpty(s), "<NULL>", s).ToString + " " : Next
-					commandlineparameters = commandlineparameters.Trim(CType(" ", Char))
-					My.App.WriteToLog("Invalid CommandLine Parameters (" + commandline.Count.ToString + ") : " + commandlineparameters)
-					My.App.SetErrorAlert()
-				End Try
-			End If
 		End Sub
 
 	End Class
